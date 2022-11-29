c468e5394ac353c2d6756f2a1bd89add93ecc025
Runas8128 <runas8128@naver.com>
2022-11-29 14:45:55 +0900

    fix: remove auth-related code in `app.py`

--- app.py
+++ app.py
@@ -31,9 +31,6 @@ def create_app(test_config=None):
     from script import db
     db.init_app(app)
 
-    from blueprint import auth
-    app.register_blueprint(auth.bp)
-
     from blueprint import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
