81ee7601b41ecaf75e614c7626469734475954f1
Runas8128 <runas8128@naver.com>
2022-12-17 01:23:44 +0900

    feature: remove `auth` and related stuffs(ex: @login_required) and following changes
    * remove `login` / `register` hyperlink
    * send `ID` text to add in `post` DB
    
    * FIXME: post divs are not showing

--- app.py
+++ app.py
@@ -32,9 +32,6 @@ def create_app(test_config=None):
     from script import db
     db.init_app(app)
 
-    from script import auth
-    app.register_blueprint(auth.bp)
-
     from script import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
--- script/blog.py
+++ script/blog.py
@@ -3,7 +3,6 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from .auth import login_required
 from .db import get_db
 
 bp = Blueprint('blog', __name__)
@@ -21,11 +20,11 @@ def index():
 
 
 @bp.route('/create', methods=('GET', 'POST'))
-@login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        user_id = request.form['user_id']
         error = None
 
         if not title:
@@ -38,7 +37,7 @@ def create():
             db.execute(
                 'INSERT INTO post (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                (title, body, user_id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -46,16 +45,16 @@ def create():
     return render_template('blog/create.html')
 
 
-def get_post(id, check_author=True):
+def get_post(post_id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
-        (id,)
+        (post_id,)
     ).fetchone()
 
     if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
+        abort(404, f"Post id {post_id} doesn't exist.")
 
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
@@ -63,10 +62,9 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    post = get_post(id)
+@bp.route('/<int:post_id>/update', methods=('GET', 'POST'))
+def update(post_id):
+    post = get_post(post_id)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -83,7 +81,7 @@ def update(id):
             db.execute(
                 'UPDATE post SET title = ?, body = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (title, body, post_id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -91,11 +89,10 @@ def update(id):
     return render_template('blog/update.html', post=post)
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    get_post(id)
+@bp.route('/<int:post_id>/delete', methods=('POST',))
+def delete(post_id):
+    get_post(post_id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM post WHERE id = ?', (post_id,))
     db.commit()
     return redirect(url_for('blog.index'))
