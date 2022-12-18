e5b7f28a6449b553cbcf01ebb9bcaca8717b9ab9
Runas8128 <runas8128@naver.com>
2022-12-18 20:22:22 +0900

    feature: remove id comparison in update/delete post

--- script/blog.py
+++ script/blog.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
@@ -12,8 +12,8 @@ bp = Blueprint('blog', __name__, url_prefix='/blog')
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        'SELECT id, title, body, created, author_id'
+        ' FROM post'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
@@ -47,17 +47,14 @@ def create():
 
 def get_post(post_id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
+        'SELECT id, title, body, created, author_id'
+        ' FROM post'
+        ' WHERE id = ?',
         (post_id,)
     ).fetchone()
 
     if post is None:
         abort(404, f"Post id {post_id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
 
     return post
 
